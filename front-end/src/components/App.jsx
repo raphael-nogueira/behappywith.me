5d3e6c8d1d82468ac78a941abcbc355a3494ef91
Raphael Nogueira <rn.rapha@gmail.com>
2025-01-03 17:20:20 -0300

    Adding toast component and logic

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 
 import Header from "./Header";
 import NovoUsuario from "./NovoUsuario";
+import Toast from "./Toast";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toast = null;
+
+        this.setToastRef = element => {
+            this.toast = element;
+        }
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <NovoUsuario />
+                <NovoUsuario erro={msg => this.toast.erro(msg)} />
+                <Toast ref={this.setToastRef}/>
             </div>
         );
     }
